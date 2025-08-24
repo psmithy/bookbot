969a75d678aa82292fde00761b06f3cf6b7d510c
psmithy <paulsmith1044@gmail.com>
2025-08-24 13:33:02 -0400

    refactor: created stats.py for word count logic

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import word_count
+
 def get_book_text(file_path):
     with open(file_path) as f:
         book_text = f.read()
     return book_text
-
-def word_count(book_text):
-    words = book_text.split()
-    word_count = len(words)
-    return word_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(book_text):
+    words = book_text.split()
+    word_count = len(words)
+    return word_count
