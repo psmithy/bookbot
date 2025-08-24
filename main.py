60edb9dac5027055b0efdd3a2a8d3c6a5081f1b9
psmithy <paulsmith1044@gmail.com>
2025-08-24 18:18:47 -0400

    Created pretty word count function

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+from stats import get_book_text
 from stats import word_count
 from stats import char_count
-
-def get_book_text(file_path):
-    with open(file_path) as f:
-        book_text = f.read()
-    return book_text
+from stats import sort_char_count
 
 def main():
+    file_path = "books/frankenstein.txt"
     book_text = get_book_text("books/frankenstein.txt")
-    num_chars = char_count(book_text)
-    print(num_chars)
+    num_words = word_count(book_text)
+    char_count_dict = char_count(book_text)
+    sorted_char_count = sort_char_count(char_count_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in sorted_char_count:
+        if not i["char"].isalpha():
+            continue
+        else:
+            print(f"{i['char']}: {i['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        book_text = f.read()
+    return book_text
+
 def word_count(book_text):
     words = book_text.split()
     word_count = len(words)
@@ -12,3 +17,14 @@ def char_count(book_text):
         if i not in char_count_dict:
             char_count_dict[i] = 1
     return char_count_dict
+
+def sort_on(char):
+    return char["num"]
+
+def sort_char_count(char_count_dict):
+    sorted_char_count = []
+    for i in char_count_dict:
+        sorted_char_count.append({"char": i, "num": char_count_dict[i]})
+    sorted_char_count.sort(reverse=True, key=sort_on)
+    return sorted_char_count
+    
